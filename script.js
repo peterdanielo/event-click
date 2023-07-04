b9aac67de40ab8ca7971b73072f5da3eca6a3202
Peter Daniel <peterdanielo@gmail.com>
2023-07-04 19:28:13 -0300

    Refatrorando o script.js

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 const cores = document.querySelectorAll('.cores ul li');
 const corSelecionada = document.querySelector('.cor-selecionada');
 
-function handleSelecionaCor(cor) {
-  const data = cor.className;
+// function handleSelecionaCor(cor) {
+//   const data = cor.className;
 
-  corSelecionada.classList.forEach((item) => {
-    if (item !== 'cor-selecionada') {
-      corSelecionada.classList.remove(item);
-    }
-  })
-  corSelecionada.classList.add(data);
+//   corSelecionada.classList.forEach((item) => {
+//     if (item !== 'cor-selecionada') {
+//       corSelecionada.classList.remove(item);
+//     }
+//   })
+//   corSelecionada.classList.add(data);
+// }
+
+// cores.forEach((cor) => {
+//   cor.addEventListener('click', () => {
+//     handleSelecionaCor(cor);
+//   });
+// });
+
+// function handleSelecionaCor({ currentTarget }) {
+//   corSelecionada.className = 'cor-selecionada';
+//   corSelecionada.classList.add(currentTarget.classList);
+//   console.log(corSelecionada, currentTarget)
+// }
+
+// cores.forEach((cor) => {
+//   cor.addEventListener('click', handleSelecionaCor);
+// });
+
+function handleSelecionaCor({ currentTarget }) {
+
+  corSelecionada.className = `cor-selecionada ${currentTarget.className}`;
 }
 
 cores.forEach((cor) => {
-  cor.addEventListener('click', () => {
-    handleSelecionaCor(cor);
-  });
+  cor.addEventListener('click', handleSelecionaCor);
 });
