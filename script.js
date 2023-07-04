2fae79c856df3d79ee0232438dadf8d21b001fc4
peterdanielo <peterdanielo@gmail.com>
2023-07-04 14:27:59 -0300

    z

---
+++ script.js
@@ -0,0 +1,20 @@
+// js
+const cores = document.querySelectorAll('.cores ul li');
+const corSelecionada = document.querySelector('.cor-selecionada');
+
+function handleSelecionaCor(cor) {
+  const data = cor.className;
+
+  corSelecionada.classList.forEach((item) => {
+    if (item !== 'cor-selecionada') {
+      corSelecionada.classList.remove(item);
+    }
+  })
+  corSelecionada.classList.add(data);
+}
+
+cores.forEach((cor) => {
+  cor.addEventListener('click', () => {
+    handleSelecionaCor(cor);
+  });
+});
